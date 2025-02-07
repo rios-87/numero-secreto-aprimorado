f35375cdad008376709899e846450dfb62938bf4
Rebert Rios <rebert.cr.rios@gmail.com>
2025-02-07 14:35:50 -0300

    Alterações no código.

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 
 //sortear(): Essa função é responsável por sortear uma quantidade específica de números aleatórios dentro de um intervalo definido pelo usuário.
+
 function sortear() { 
 let quantidadeDeNumeros = parseInt (document.getElementById('quantidade').value); // parseInt() garante que os valores capturados sejam convertidos para números inteiros
 let numeroInicial = parseInt (document.getElementById('de').value);
 let numeroFinal = parseInt (document.getElementById('ate').value);
-
 let sorteados =[]; // É um array onde os números sorteados serão armazenados.
 let numero;        // Será utilizado para armazenar temporariamente os valores gerados.
 
+if (numeroInicial >= numeroFinal) {
+  alert (`O número inicial é maior que o número Final, por gentileza, informar um valor menor no campo:"Do número"!`);
+} else {quantidadeDeNumerosSorteados();  }
+
+
+function quantidadeDeNumerosSorteados() {
 //FOR: Loop para gerar números aleatórios
 //O loop "for" executa um número de vezes igual ao valor inserido pelo usuário (quantidade de números a sortear).
-
 for (let i = 0; i < quantidadeDeNumeros; i++){
   numero = gerarNumeroAleatorio(numeroInicial,numeroFinal);
-  //sorteados.push(numero);
 
   while (sorteados.includes(numero) ){
-    numero = gerarNumeroAleatorio(numeroInicial,numeroFinal);
-    
+    numero = gerarNumeroAleatorio(numeroInicial,numeroFinal);    
   }
   sorteados.push(numero);
 }
-
 let resultado = document.getElementById('resultado');
  resultado.innerHTML = (`<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`);
 alterarStatusBotao();
+ }
 }
 
 //Chama a função auxiliar para gerar um número aleatório dentro do intervalo definido.
@@ -50,3 +53,4 @@ function reiniciar () {
   document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
   alterarStatusBotao();
 }
+
